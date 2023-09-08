77e9f5e4d29cbc1b6b8e24a23652c6928e5a205c
Duncan <chirchirduncan@outlook.com>
2023-09-08 14:57:33 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/* the code below will print whether the number is positive or negative */
+/**
+*this code will print whether the numer stored is positive or negative
+*Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -10,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n == 0)
-                printf("%d is zero\n", n);
-        else 
-                printf("%d is negative\n", n);
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 	return (0);
 }
