a1f35dfb6c183dd8d6686b44ece7e787e8e45463
Duncan <chirchirduncan@outlook.com>
2023-09-18 08:59:12 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - this function prints a string,
+ * followed by a new line.
+ * @str: Variable string.
+ *
+ * Return: Always 0 (success)
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar(10);
+}
