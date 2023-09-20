4b037efbd19fbc69b057dc9f26642f878e76610e
Duncan <chirchirduncan@outlook.com>
2023-09-20 09:47:01 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncpy - function that copies the string,
+ * followed by a new line.
+ * @dest: array name.
+ * @src: elements of the array.
+ * @n: number of bytes.
+ *
+ * Return: Array of characters.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n ; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+	_putchar('\n');
+}
