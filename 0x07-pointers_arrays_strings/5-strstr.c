2b6c8a9a2189eed3a6c5bd2666aba45c182b815f
Duncan <chirchirduncan@outlook.com>
2023-09-25 05:58:08 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring,
+ * followed by a new line.
+ * @haystack: is the string.
+ * @needle: is the substring.
+ *
+ * Return: pointer to beginning of needle in @haystack or
+ * NULL if no substring is found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
