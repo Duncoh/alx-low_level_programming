541db0bc92e2660449af3633c60da69de2606b75
Duncan <chirchirduncan@outlook.com>
2023-09-29 07:22:42 -0700

    3-strspn.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strspn - a function that gets the length of a prefix substring,
+ * followed by a new line.
+ * @s: initial segment.
+ * @accept: Pointer to a string.
+ *
+ * Return: Length of a prefix substring.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	unsigned int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0 ; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
+		if (s[i] != accept[j])
+		{
+			break;
+		}
+	}
+	return (count);
+}
